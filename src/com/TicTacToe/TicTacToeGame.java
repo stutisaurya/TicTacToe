7790da3b799bbd3bde1fc8a53220ff9a618e4f56
stutisaurya <stuti.saurya@gmail.com>
2021-06-29 16:07:10 +0530

    Added:GoToDesiredLocation

--- src/com/TicTacToe/TicTacToeGame.java
+++ src/com/TicTacToe/TicTacToeGame.java
@@ -3,8 +3,10 @@ package com.TicTacToe;
 import java.util.Scanner;
 
 public class TicTacToeGame {
-	static String turn;
+	static char turn;
+	static Scanner userinput;
 
+	// * UC1 *
 	public static char[] createBoard() {
 		char[] gameBoard = new char[10];
 		for (int i = 1; i < 10; i++) {
@@ -13,21 +15,23 @@ public class TicTacToeGame {
 		return gameBoard;
 	}
 
-	public static String choosePlayerTurn() {
+	// * UC2 *
+	public static char choosePlayerTurn() {
 		System.out.println("Write the play key");
-		Scanner sc = new Scanner(System.in);
-		turn = sc.nextLine(); // X is for computer player and O for User player
-		if (turn.equals("X")) {
+		Scanner userinput = new Scanner(System.in);
+		turn = userinput.next().toUpperCase().charAt(0); // X is for computer player and O for User player
+		if (turn == 'X') {
 			System.out.println("Computer is playing");
-			turn = "O"; // Computer is playing so now it is turn to play user
+			turn = 'O'; // Computer is playing so now it is turn to play user
 			return turn;
 		} else {
 			System.out.println("User is playing");
-			turn = "X"; // User is playing so now it is turn to play computer
+			turn = 'X'; // User is playing so now it is turn to play computer
 			return turn;
 		}
 	}
 
+	// * UC3 *
 	public static void showBoard(char[] board) {
 		System.out.println("/---|---|---\\");
 		System.out.println("| " + board[1] + " | " + board[2] + " | " + board[3] + " |");
@@ -38,10 +42,39 @@ public class TicTacToeGame {
 		System.out.println("/---|---|---\\");
 	}
 
+	// * UC4 *
+	public static void moveToDesiredLocation(char[] board) {
+		Scanner input = new Scanner(System.in);
+		int numInput;
+		int j = 0;
+		while (j == 0) {
+			System.out.println("Please enter a number between 1 to 9");
+			numInput = input.nextInt();
+			if (!(numInput > 0 && numInput <= 9)) // check the input is between 1 to 9 or not
+			{
+				System.out.println("Invalid input; re-enter slot number:");
+				j = 1;
+				continue;
+
+			} else if (board[numInput] == ' ') {
+				board[numInput] = turn;
+				showBoard(board);
+				j = 1;
+			} else {
+				System.out.println("Slot already taken; re-enter slot number:");
+				j = 1;
+				continue;
+
+			}
+		}
+	}
+
 	public static void main(String[] args) {
+		Scanner userinput = new Scanner(System.in);
 		char[] board = createBoard(); // Method for creating Empty board for game
 		System.out.println("Welocme to User And Computer Player");
-		String turn = choosePlayerTurn(); // Method for choose player turn
-		showBoard(board);
+		char turn = choosePlayerTurn(); // Method for choose player turn
+		showBoard(board); // Method to show board of Tic Tac Toe toe game
+		moveToDesiredLocation(board); // Method to go desired location to user in game
 	}
 }
